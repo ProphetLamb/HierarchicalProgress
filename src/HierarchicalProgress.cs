b334b772d5d8b9e46b526079fcf9b286af81e26a
ProphetLamb <rene.carannante@outlook.de>
2021-06-20 23:59:54 +0200

    Inherit from HierarchicalProgressBase

--- src/HierarchicalProgress.cs
+++ src/HierarchicalProgress.cs
@@ -1,119 +1,167 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 using GenericRange;
+using GenericRange.Extensions;
+
+using HierarchicalProgress.Base;
+using HierarchicalProgress.DebugViews;
+using HierarchicalProgress.Exceptions;
 
 namespace HierarchicalProgress
 {
     [DebuggerDisplay("{GetDebuggerDisplay(),nq}")]
-    public class HierarchicalProgress<T> : IHierarchicalProgress<T> where T : IProgressValue
+    [DebuggerTypeProxy(typeof(EnumerableDebugView<>))]
+    public partial class HierarchicalProgress<TProgressReport> : HierarchicalProgressBase<TProgressReport>
+        where TProgressReport : IProgressReport, new()
     {
 #region Ctor
 
-        public HierarchicalProgress(double maximumProgress)
-            : this(new Range<double>(0, 0, true), maximumProgress) { }
-
-        public HierarchicalProgress(in Range<double> progressRange, double maximumProgress)
-        {
-            if (progressRange.Start.GetOffset(maximumProgress) < 0)
-                throw new ArgumentOutOfRangeException(nameof(progressRange), "Start value must be greater then or equal to zero.");
-            if (progressRange.End.GetOffset(maximumProgress) > maximumProgress)
-                throw new ArgumentOutOfRangeException(nameof(progressRange), "End value must be less then or equal to " + nameof(MaximumProgress));
-            
-            ProgressRange = progressRange;
-            MaximumProgress = maximumProgress;
-        }
-
+        public HierarchicalProgress(Range<double> progressBoundaries, Range<double> reportBoundaries)
+            : base(progressBoundaries, reportBoundaries)
+        { }
+        
 #endregion
         
 #region Fields
-        
-        private WeakEventDispatcher<ProgressChangedEventArgs<T>> _dispatcher = new();
-        private T? _current;
 
-#endregion
-
-#region Properties
-
-        public event EventHandler<ProgressChangedEventArgs<T>> ProgressChanged
-        {
-            add => _dispatcher += value;
-            remove => _dispatcher -= value;
-        }
-        
-        public Range<double> ProgressRange { get; }
-
-        public double MaximumProgress { get; }
-        
-        /// <summary>
-        ///     The latest <see cref="IProgress{T}.Report"/>ed value.
-        /// </summary>
-        /// <exception cref="ArgumentNullException">The <paramref name="value"/> is <see langword="null"/>.</exception>
-        public virtual T? Current
-        {
-            get => _current;
-            protected set
-            {
-                if (value == null)
-                    throw new ArgumentNullException(nameof(value));
-                _current = value;
-                OnProgressChanged(value);
-            }
-        }
-
-        public bool ThrowOnSliceOverMaximum { get; set; } = true;
+        private readonly List<IObserver<TProgressReport>> _observers = new();
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        private readonly object _sliceObserverSyncLock = new();
 
 #endregion
 
 #region Public members
-
-        public virtual void Report(T value)
-        {
-            Range<double> virtualRange = GetVirtualRange();
-            if (virtualRange.ContainsEndInclusive(value.Progress))
-                throw new ArgumentOutOfRangeException(nameof(value), "Progress must be within the range " + virtualRange);
-            Current = value;
-        }
-
-        public virtual IHierarchicalProgress<T> Slice(Range<double> range)
-        {
-            if (!ProgressRange.Encompasses(range, MaximumProgress))
-                throw new ArgumentOutOfRangeException(nameof(range), "Value must be encompassed by the range " + ProgressRange);
-            
-            HierarchicalProgress<T> progress = new(range, MaximumProgress);
-            progress.ProgressChanged += SlicedProgressChanged;
-            return progress;
-        }
-
-        public override string ToString() => $"{nameof(HierarchicalProgress<T>)}: {{ {GetDebuggerDisplay()} }}";
         
+        public override HierarchicalProgress<TProgressReport> Slice(Range<double> reportBoundaries, double allocateProgress)
+        {
+            double totalAvailableProgress = ProgressBoundaries.GetOffsetAndLength().Length;
+            double freeProgress = totalAvailableProgress - AllocatedProgress - allocateProgress;
+            if (freeProgress < 0)
+                throw new ArgumentOutOfRangeException(nameof(allocateProgress), "Insufficient progressValue to allocate.");
+            
+            // Calculate new ReportBoundaries
+            double freeProgressPercent = freeProgress / totalAvailableProgress;
+            (double reportOff, double reportLen) = OriginReportBoundaries.GetOffsetAndLength();
+            
+            ReportBoundaries = new Range<double>(reportOff, reportOff + reportLen * freeProgressPercent);
+            AllocatedProgress -= allocateProgress;
+
+            Range<double> sliceProgressBoundaries = new(ProgressBoundaries.Start.Value, ProgressBoundaries.Start.Value + allocateProgress);
+            var slice = new HierarchicalProgress<TProgressReport>(sliceProgressBoundaries, reportBoundaries);
+            
+            SliceObserver observer = new(this, slice);
+            observer.Unsubscriber = Subscribe(observer);
+
+            return slice;
+        }
+
+        public override IDisposable Subscribe(IObserver<TProgressReport> observer)
+        {
+            if (!_observers.Contains(observer))
+            {
+                _observers.Add(observer);
+                foreach (TProgressReport report in m_progressReports)
+                    observer.OnNext(report);
+            }
+
+            return new Unsubscriber(_observers, observer);
+        }
+
 #endregion
 
 #region Internal members
 
-        protected virtual void SlicedProgressChanged(object? sender, ProgressChangedEventArgs<T> args)
+        protected override void InternalReport(TProgressReport? report, ProgressChange change)
         {
-            T sliceProgress = (T)args.Value.Clone();
-            sliceProgress.Progress = EnsureProgressChangedArgs(args);
-            Current = sliceProgress;
+            ThrowIfCompleted(IsCompleted, change);
+            // Only completion can be done without a report.
+            Debug.Assert(report != null || change == ProgressChange.Completed, "report != null || change == ProgressChange.Completed");
+            
+            double progress = change switch {
+                ProgressChange.Completed => ProgressBoundaries.End.Value,
+                ProgressChange.Reset => ProgressBoundaries.Start.Value,
+                _ => ReportBoundaries.Map(ProgressBoundaries, report!.ReportProgress)
+            };
+            
+            if (!ProgressBoundaries.Contains(progress))
+                throw new IndexOutOfRangeException($"The index {progress} is outside of the range {ProgressBoundaries}.");
+            
+            TProgressReport? previous = LatestReport;
+            LatestChange = (change, progress - Progress.Value);
+            Progress = progress;
+            LatestReport = report;
+            OnChanged(previous, report, change);
         }
 
-        protected virtual void OnProgressChanged(T value) => _dispatcher.Invoke(this, new ProgressChangedEventArgs<T>(value, ProgressRange));
-
-        private string GetDebuggerDisplay() => $"{nameof(ProgressRange)}: {ProgressRange}, {nameof(MaximumProgress)}: {MaximumProgress}, {nameof(Current)}: {Current}";
-
-        private double EnsureProgressChangedArgs(ProgressChangedEventArgs<T> args)
+        /// <summary>Notifies all listeners when a progress changes is reported.</summary>
+        protected virtual void OnChanged(TProgressReport? previousProgress, TProgressReport? reportedProgress, ProgressChange change)
         {
-            double progress = args.ProgressRange.GetOffsetAndLength(MaximumProgress).Offset + args.Value.Progress;
-            Range<double> virtualRange = GetVirtualRange();
-            if (virtualRange.ContainsEndInclusive(progress))
-                return progress;
-            if (ThrowOnSliceOverMaximum)
-                throw new ArgumentOutOfRangeException(nameof(args), "Value is not within the progress range " + virtualRange);
-            return ProgressRange.GetOffsetAndLength(MaximumProgress).Length;
+            // Only completion can be done without a report.
+            Debug.Assert(reportedProgress != null || change == ProgressChange.Completed, "report != null || change == ProgressChange.Completed");
+
+            foreach (IObserver<TProgressReport> observer in _observers)
+            {
+                if (reportedProgress != null)
+                    observer.OnNext(reportedProgress);
+                if (change == ProgressChange.Completed)
+                    observer.OnCompleted(); // Clear the _observers list
+            }
+
+            if (reportedProgress != null)
+            {
+                m_progressReports.Add(reportedProgress);
+
+                OnReported(previousProgress, reportedProgress, change);
+            }
+            
+            switch (change)
+            {
+                case ProgressChange.Reset:
+                    OnReset(previousProgress, reportedProgress);
+                    break;
+                case ProgressChange.Completed:
+                    OnCompleted(previousProgress, reportedProgress);
+                    break;
+            }
         }
 
-        private Range<double> GetVirtualRange() => new(0, ProgressRange.GetOffsetAndLength(MaximumProgress).Length);
+        protected override ProgressChange GetReportProgressChange(TProgressReport? previous, TProgressReport report)
+        {
+            ProgressChange changeType = previous?.ReportProgress.CompareTo(report.ReportProgress) switch {
+                < 0 => ProgressChange.Increment,
+                0 => ProgressChange.None,
+                > 0 => ProgressChange.Decrement,
+                null => ProgressChange.Increment
+            };
+            if (report.ReportProgress - Double.Epsilon <= ProgressBoundaries.Start.Value)
+                changeType = ProgressChange.Reset;
+            else if (report.ReportProgress >= ProgressBoundaries.End.Value - Double.Epsilon)
+                changeType = ProgressChange.Completed;
+            return changeType;
+        }
+        
+        /// <summary>Routes the <see cref="TProgressReport"/> with the specified <paramref name="progressValue"/> thought the <see cref="HierarchicalProgress{TProgressReport}"/>.</summary>
+        /// <param name="progressValue">The progress index within <see cref="HierarchicalProgressBase{TProgressReport}.ProgressBoundaries"/>.</param>
+        /// <param name="inner">The report to route.</param>
+        /// <returns>The routed report.</returns>
+        /// <remarks>Called when a change in TProgressReport is observed in a slice.</remarks>
+        protected virtual TProgressReport Route(Index<double> progressValue, TProgressReport inner)
+        {
+            TProgressReport routed = new() {
+                ReportProgress = ReportBoundaries.Map(ProgressBoundaries, progressValue),
+                Inner = inner
+            };
+            return routed;
+        }
+
+        private static void ThrowIfCompleted([DoesNotReturnIf(true)] bool isCompleted, ProgressChange reportChange)
+        {
+            if (isCompleted)
+                throw new InvalidProgressStateException("A completed progress provider can no longer receive reports.", reportChange, null);
+        }
 
 #endregion
     }
