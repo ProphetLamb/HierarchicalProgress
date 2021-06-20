df7f9755b0f578e788334c4d45d23841295e1991
ProphetLamb <rene.carannante@outlook.de>
2021-06-21 00:00:14 +0200

    Unsubscriber

---
+++ src/HierarchicalProgress.Unsubscriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HierarchicalProgress
+{
+    partial class HierarchicalProgress<TProgressReport> where TProgressReport : IProgressReport, new()
+    {
+        internal sealed class Unsubscriber : IDisposable
+        {
+            private IList<IObserver<TProgressReport>> _observers;
+            private IObserver<TProgressReport> _observer;
+
+            internal Unsubscriber(IList<IObserver<TProgressReport>> observers, IObserver<TProgressReport> observer)
+            {
+                _observers = observers;
+                _observer = observer;
+            }
+
+            public void Dispose()
+            {
+                if (_observers.Contains(_observer))
+                    _observers.Remove(_observer);
+            }
+        }
+    }
+}
