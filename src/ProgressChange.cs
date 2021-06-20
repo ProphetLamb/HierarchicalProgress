f2e5cb042697537b58d89a696624718d8fdea67e
ProphetLamb <rene.carannante@outlook.de>
2021-06-21 00:02:15 +0200

    Create ProgressChange

---
+++ src/ProgressChange.cs
@@ -0,0 +1,19 @@
+﻿namespace HierarchicalProgress
+{
+    /// <summary>
+    /// Describes the manner in which the progress has changed between reports.
+    /// </summary>
+    public enum ProgressChange : byte
+    {
+        /// <summary>The progress has not changed.</summary>
+        None,
+        /// <summary>The progress has progressed.</summary>
+        Increment,
+        /// <summary>The progress has regressed.</summary>
+        Decrement,
+        /// <summary>The progress is reset to the initial progress. Usually zero.</summary>
+        Reset,
+        /// <summary>The progress is completed.</summary>
+        Completed
+    }
+}
