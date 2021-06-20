d147b0e5fda4e859e47a5965e0a5477e54d38913
ProphetLamb <rene.carannante@outlook.de>
2021-06-20 23:56:46 +0200

    Implement debug view

---
+++ src/DebugViews/EnumerableDebugView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace HierarchicalProgress.DebugViews
+{
+    internal sealed class EnumerableDebugView<T>
+    {
+        private IEnumerable<T> _sequence; 
+        
+        public EnumerableDebugView(IEnumerable<T> sequence) {
+            _sequence = sequence;
+        }
+       
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public T[] Items => _sequence.ToArray();
+    }
+}
